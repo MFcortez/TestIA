35d0dff77715f62a02e55a55066f1532fc5191f4
MFcortez <mep.filho2012@gmail.com>
2022-04-02 16:29:33 -0300

    Suavemente Comentando

--- Assets/Scripts/DirectionManager.cs
+++ Assets/Scripts/DirectionManager.cs
@@ -5,31 +5,31 @@ using UnityEngine;
 public class DirectionManager : MonoBehaviour
 {
     public Movement movement;
-    public GameObject[] pointsOfInterest;
-    int actPoint;
-    public bool four;
+    public GameObject[] pointsOfInterest; //array com os destinos da cena
+    int actPoint; //destino atual baseado na sua posiçao no array
+    public bool four; //bool para definir se e a cena 4 ou nao
 
     void Start()
     {
-        movement = GetComponent<Movement>();
-        actPoint = 0;
+        movement = GetComponent<Movement>(); //pega o script movement do objeto e guarda na variavel movement
+        actPoint = 0; //define o destino atual para zero
     }
 
     void Update()
     {
-        if (transform.position == pointsOfInterest[actPoint].transform.position)
+        if (transform.position == pointsOfInterest[actPoint].transform.position)//se o objeto chegou ao destino faça
         {
-            if (four)
+            if (four) //se for a cena 4
             {
-                pointsOfInterest[actPoint].GetComponent<Renderer>().material.color = Color.blue;
+                pointsOfInterest[actPoint].GetComponent<Renderer>().material.color = Color.blue; //muda a cor do gameobject de waypoint
             }
-            actPoint++;
-            if(actPoint >= 5)
+            actPoint++; //passa para o proximo objeto do array
+            if(actPoint >= 5) //sao 5 waypoints na cena, caso ele já tenha passado pelo waypoint 4 (o ultimo no array)
             {
-                actPoint = 0;
+                actPoint = 0; //volta para o primeiro objeto do array
             }
-            movement.ponto = pointsOfInterest[actPoint];
-            movement.Reset();
+            movement.ponto = pointsOfInterest[actPoint]; //muda o destino do script Movement
+            movement.Reset(); //chama a funçao reset do script movement para se adaptar ao proximo destino
         }
     }
 }
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,38 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//inGameTempo = tempo passado desde que o jogo foi iniciado
+
 public class Movement : MonoBehaviour
 {
-    public float velocity;
-    public GameObject ponto;
-    Transform thisTransform;
-    Vector3 initialPos;
-    float distance, percorrido, startTime;
+    public float velocity; //velocidade do objeto
+    public GameObject ponto; //destino
+    Transform thisTransform; 
+    Vector3 initialPos; //posiçao inicial
+    
+    //floats para calcular variação do movimento
+    float distance, 
+        percorrido, 
+        startTime;
 
     void Start()
     {
-        startTime = Time.time;
+        startTime = Time.time; //define o momento inGameTempo em que o objeto começa a se mover
         thisTransform = GetComponent<Transform>();
-        initialPos = thisTransform.position;
-        distance = Vector3.Distance(thisTransform.position, ponto.transform.position);
+        initialPos = thisTransform.position; //define a posição inicial do objeto
+        distance = Vector3.Distance(initialPos, ponto.transform.position); //pega a distancia entre a posiçao inicial do objeto e seu destino
     }
 
-    public void Reset()
+    public void Reset() //chamado ao mudar de direção
     {
-        startTime = Time.time;
-        thisTransform = GetComponent<Transform>();
-        initialPos = thisTransform.position;
-        distance = Vector3.Distance(thisTransform.position, ponto.transform.position);
+        startTime = Time.time; //define o momento inGameTempo em que o objeto reseta
+        initialPos = thisTransform.position; //redefine a posição inicial do objeto para a posição atual
+        distance = Vector3.Distance(initialPos, ponto.transform.position); //redefine a distancia entre a posiçao inicial e o destino
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        percorrido = (Time.time - startTime) * velocity;
-        float fracao = percorrido / distance;
+        percorrido = (Time.time - startTime) * velocity; //define a posição atual do objeto em relaçao ao tempo e sua velocidade
+        float fracao = percorrido / distance; //define quanto do caminho ja foi percorrido (0 = inicio / 100 = destino)
 
-        if(thisTransform.position != ponto.transform.position)
+        if(thisTransform.position != ponto.transform.position) //se o objeto ainda nao chegou ao destino faça
         {
-            thisTransform.position = Vector3.Lerp(initialPos, ponto.transform.position, fracao);
+            thisTransform.position = Vector3.Lerp(initialPos, ponto.transform.position, fracao); //define a posiçao atual do objeto ate ele chegar no destino
         }
     }
 }
